26b1b38e06bcf99916eb0e4077a573b18693e232
IntelliRon <gulpsabook@gmail.com>
2026-07-26 23:28:06 +0100

    chore: Remove server auth debug

--- Web/server/src/routes/auth.js
+++ Web/server/src/routes/auth.js
@@ -96,8 +96,6 @@ router.post("/login", async (req, res) => {
 	try {
 		const { username, password } = req.body || {};
 
-		console.log(req.body);
-
 		if (!username || !password) {
 			return res.status(400).json({
 				success: false,
